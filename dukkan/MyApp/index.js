d4bd5efb33c52c0cd4acc6add19016175cece589
Burçin Güngör <159061359+burcingungorr@users.noreply.github.com>
2025-04-15 16:00:32 +0300

    ...

--- dukkan/MyApp/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-import 'react-native-gesture-handler'
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import Wrapper from './src/Wrapper';
-
-AppRegistry.registerComponent(appName, () => Wrapper);
